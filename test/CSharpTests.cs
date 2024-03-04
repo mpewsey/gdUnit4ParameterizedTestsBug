b6cbf0ec208295f0c4f76dbd81142d1e9265b457
Matt Pewsey <23442063+mpewsey@users.noreply.github.com>
2024-03-03 22:14:41 -0500

    Add test scene

--- test/CSharpTests.cs
+++ test/CSharpTests.cs
@@ -10,4 +10,13 @@ public class CSharpTests
     {
         Assertions.AssertThat(a + b + c).IsEqual(expect);
     }
+
+    [TestCase(1, 2, 3, 6)]
+    [TestCase(3, 4, 5, 12)]
+    [TestCase(6, 7, 8, 21)]
+    public void TestCaseArgumentsWithScene(int a, int b, int c, int expect)
+    {
+        ISceneRunner.Load("res://test_scene.tscn", true, true);
+        Assertions.AssertThat(a + b + c).IsEqual(expect);
+    }
 }
